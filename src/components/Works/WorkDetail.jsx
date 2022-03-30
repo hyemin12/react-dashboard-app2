ea8de2fdf54065186886839f144c67f2cf00bbf3
hyemin12 <khnm31@naver.com>
2022-03-30 13:46:36 +0900

    '데이터추가'

--- src/components/Works/WorkDetail.jsx
+++ src/components/Works/WorkDetail.jsx
@@ -2,9 +2,11 @@ import { FaCode, FaEdge } from "react-icons/fa";
 import { useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
 import "./workdetail.css";
+import { useState } from "react";
 
 function WorkDetail() {
   const { id } = useParams();
+  const [isTab, setIsTab] = useState(true);
   const state = useSelector((state) => state.worksReducer);
   const index = state.findIndex((element) => element.id === id);
   const item = state[index];
@@ -25,20 +27,49 @@ function WorkDetail() {
             })}
           </div>
 
-          <div className="link-group">
-            <a href={item.code} target="_blank" rel="noreferrer">
-              <FaCode />
-              <span>소스 보기</span>
-            </a>
-            <a href={item.site} target="_blank" rel="noreferrer">
-              <FaEdge />
-              <span>사이트 바로가기</span>
-            </a>
-          </div>
+          {item.code ? (
+            <div className="link-group">
+              <a href={item.code} target="_blank" rel="noreferrer">
+                <FaCode />
+                <span>소스 보기</span>
+              </a>
+              <a href={item.site} target="_blank" rel="noreferrer">
+                <FaEdge />
+                <span>사이트 바로가기</span>
+              </a>
+            </div>
+          ) : null}
         </div>
         {item.img2 != null ? (
           <div className="img2">
             <img src={`../img/portfolio/${item.img2}.jpg`} alt={item.title} />
+          </div>
+        ) : null}
+        {item.tab1 !== null ? (
+          <div className="detail-tab-wrapper">
+            <ul className="tab-title">
+              <li
+                onClick={() => {
+                  setIsTab(true);
+                }}
+              >
+                리디자인
+              </li>
+              <li
+                onClick={() => {
+                  setIsTab(false);
+                }}
+              >
+                원사이트
+              </li>
+            </ul>
+            <div className="tab-img">
+              {isTab ? (
+                <img src={`../img/portfolio/${item.tab}`} alt={item.title} />
+              ) : (
+                <img src={`../img/portfolio/${item.tab2}`} alt={item.title} />
+              )}
+            </div>
           </div>
         ) : null}
       </div>
